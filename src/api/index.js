1c4976154337abc7eaac284a3079570a0a7e800d
Filip R <ruszala.filip@gmail.com>
2023-04-06 21:28:21 +0200

    all done

---
+++ src/api/index.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, ne, sw) => {
+  console.log(type);
+  try {
+    const {
+      data: { data },
+    } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "X-RapidAPI-Key": "1c6ae325a2mshb767c93d6f1cc38p1602fejsn87d2ae0f67a8",
+        "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+      },
+    });
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
